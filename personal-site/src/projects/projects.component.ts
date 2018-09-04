3e1e49efae0fc5fb6bb8c7d0a0d9e442e301ccf1
amyyan210 <amy@yanaway.com>
2018-09-04 17:39:12 -0500

    Made projects page dynamic using Angular

--- personal-site/src/projects/projects.component.ts
+++ personal-site/src/projects/projects.component.ts
@@ -13,57 +13,67 @@ export class ProjectsComponent implements OnInit {
     {
       name: 'Inspiration Camps',
       site: 'https://www.inspirationcamps.org',
-      img: '../../src/assets/img/inspiration.png'
+      img: '../../src/assets/img/inspiration.png',
+      type: 'Tech'
     },
     {
       name: 'Remindr',
       site: 'http://remindr.life',
       img: '../../src/assets/img/remindr.png',
-      github: 'https://github.com/SweetJandy/Remindr'
+      github: 'https://github.com/SweetJandy/Remindr',
+      type: 'Tech'
     },
     {
       name: 'Spooklist',
       img: '../../src/assets/img/spooklist.png',
-      github: 'https://github.com/brauly/adlister'
+      github: 'https://github.com/brauly/adlister',
+      type: 'Tech'
     },
     {
       name: 'Phone Book',
       site: '../phone-book.jar',
       img: '../../src/assets/img/phone-book.png',
       github: 'https://github.com/amyyan210/phone-book',
-      special: '<p class="text-center" id="run-instructions">*Download and run <code>java -jar phone-book.jar</code> in terminal</p>'
+      special: true,
+      type: 'Tech'
     },
     {
       name: 'Movie Database',
       img: '../../src/assets/img/movies.jpg',
-      github: 'https://github.com/movie-api/bramy'
+      github: 'https://github.com/movie-api/bramy',
+      type: 'Tech'
     },
     {
       name: 'Weather Forecast',
       site: '',
       img: '../assets/img/weather.jpg',
-      github: 'https://github.com/PairCoding/Amy_Edward/tree/weather-api'
+      github: 'https://github.com/PairCoding/Amy_Edward/tree/weather-api',
+      type: 'Tech'
     },
     {
       name: 'Calculator',
       site: '',
       img: '../assets/img/calculator.png',
-      github: 'https://github.com/calculator-project/calculator-project'
+      github: 'https://github.com/calculator-project/calculator-project',
+      type: 'Tech'
     },
     {
       name: 'Thesis',
       site: 'https://uh-ir.tdl.org/uh-ir/bitstream/handle/10657/1150/YANAWAY-THESIS-2015.pdf?sequence=1&isAllowed=y',
-      img: '../assets/img/thesis.png'
+      img: '../assets/img/thesis.png',
+      type: 'Other'
     },
     {
       name: 'Variations',
       site: 'https://youtu.be/VjbkGdkpm4w',
-      img: '../assets/img/variations.png'
+      img: '../assets/img/variations.png',
+      type: 'Other'
     },
     {
       name: 'Arrangement',
       site: 'https://youtu.be/9DvNEo_X-7A',
-      img: '../assets/img/saudades.png'
+      img: '../assets/img/saudades.png',
+      type: 'Other'
     }
   ];
 
